0156998ef37a994ad461d72a2945bf09d17d0429
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-04-18 15:14:30 +0200

    Reset style and routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import styled from 'styled-components';
 
 const StyledTitle = styled.h1`
@@ -9,45 +7,42 @@ font-size: 26px;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <StyledTitle>
-          Wyjątkowy prezent
-        </StyledTitle>
-        <p>Wyobraź sobie, że możesz mieć swoją własną, unikalną figurkę 3D stworzoną na podstawie Twojego zdjęcia.
+      <StyledTitle>
+        Wyjątkowy prezent
+      </StyledTitle>
+      <p>Wyobraź sobie, że możesz mieć swoją własną, unikalną figurkę 3D stworzoną na podstawie Twojego zdjęcia.
 
-          To możliwe dzięki naszej usłudze personalizowanych figur 3D! Idealne jako prezent dla bliskiej osoby lub jako pamiątka z ważnego wydarzenia.
+        To możliwe dzięki naszej usłudze personalizowanych figur 3D! Idealne jako prezent dla bliskiej osoby lub jako pamiątka z ważnego wydarzenia.
 
-          Nie czekaj dłużej i zamów już dziś!</p>
-        <p>Zamawiam</p>
+        Nie czekaj dłużej i zamów już dziś!</p>
+      <p>Zamawiam</p>
 
 
-        <div>
-          <h2>
-            Unikalność
-          </h2>
-          <p>
-            Każda figurka jest tworzona na podstawie zdjęcia klienta, co oznacza, że każda z nich jest całkowicie unikalna i osobista.
-          </p>
-        </div>
-        <div>
-          <h2>
-            Wysoka jakość
-          </h2>
-          <p>
-            Nasze figurki 3D są drukowane z najwyższej jakości materiałów, co gwarantuje trwałość i długotrwałe użytkowanie.
-          </p>
-        </div>
-        <div>
-          <h2>
-            Doskonały prezent
-          </h2>
-          <p>
-            Personalizowane figurki 3D to idealny prezent dla każdego. Możesz zamówić figurkę na podstawie zdjęcia bliskiej osoby i sprawić jej niezapomnianą niespodziankę.
-          </p>
-        </div>
-        <div> Zamów już dziś unikalną figurkę</div>
-      </header>
+      <div>
+        <h2>
+          Unikalność
+        </h2>
+        <p>
+          Każda figurka jest tworzona na podstawie zdjęcia klienta, co oznacza, że każda z nich jest całkowicie unikalna i osobista.
+        </p>
+      </div>
+      <div>
+        <h2>
+          Wysoka jakość
+        </h2>
+        <p>
+          Nasze figurki 3D są drukowane z najwyższej jakości materiałów, co gwarantuje trwałość i długotrwałe użytkowanie.
+        </p>
+      </div>
+      <div>
+        <h2>
+          Doskonały prezent
+        </h2>
+        <p>
+          Personalizowane figurki 3D to idealny prezent dla każdego. Możesz zamówić figurkę na podstawie zdjęcia bliskiej osoby i sprawić jej niezapomnianą niespodziankę.
+        </p>
+      </div>
+      <div> Zamów już dziś unikalną figurkę</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Order from './pages/order';
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/order" component={Order} />
-    </Switch>
-  </Router>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
